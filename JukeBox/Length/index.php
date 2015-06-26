e857f25d68624d200a21bdb362aa29d2d2a85885
dantheradarman <dantheradarman@gmail.com>
2015-06-26 17:24:38 +0100

    Update index.php

--- JukeBox/Length/index.php
+++ JukeBox/Length/index.php
@@ -1,4 +1,11 @@
-<?php
-  $check = http_get("https://gdata.youtube.com/feeds/api/videos/GZMzd4PmV1w?v=2", array(), $info);
-  print_r($info);
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+      $check = http_get("https://gdata.youtube.com/feeds/api/videos/GZMzd4PmV1w?v=2", array(), $info);
+      print_r($info);
+    ?>
+  </body>
+</html>
